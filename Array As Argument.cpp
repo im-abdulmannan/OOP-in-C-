a61f9911f0ce8da039821152a24d672e708a7c61
Abdul Mannan <abdulmann1234@gmail.com>
2023-01-31 14:52:33 +0500

    last commit

---
+++ Array As Argument.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int size)
+{
+    cout << "Array is: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << ", ";
+    }
+}
+
+int main()
+{
+    int A[] = {1, 2, 3, 4, 5, 6, 7};
+    int size = sizeof(A) / sizeof(A[0]);
+    printArray(A, size);
+    return 0;
+}
