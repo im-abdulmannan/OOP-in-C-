0eec0a7cb5a0099b4c7f14184b3285e0035f4186
Abdul Mannan <abdulmann1234@gmail.com>
2023-02-07 21:03:49 +0500

    first commit

--- Box Pattern 1.cpp
+++ Box Pattern 1.cpp
@@ -2,22 +2,25 @@
 using namespace std;
 
 int main() {
-    for(int a = 0; a <= 8; a++) {
-        cout << "*";
-    }
-    cout << endl;
+    for (int i = 0; i < 5; i++) {
+        for (int a = 0; a <= 4; a++) {
+            cout << "* ";
+        }
+        cout << endl;
 
-    for (int b = 0; b <= 2; b++) {
-        for(int c = 0; c < 1; c++) {
-            cout << "*";
+        for (int b = 0; b < 2; b++) {
+            for (int c = 0; c < 1; c++) {
+                cout << "*";
+            }
+            for (int d = 0; d <= 6; d++) {
+                cout << " ";
+            }
+            cout << "*" << endl;
         }
-        for (int d = 0; d <= 6; d++) {
-            cout << " ";
-        }
-        cout << "*" << endl;
     }
-    for(int e = 0; e <= 8; e++) {
-        cout << "*";
+
+    for (int e = 0; e <= 4; e++) {
+        cout << "* ";
     }
 
     return 0;
--- Box Pattern.cpp
+++ Box Pattern.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    for(int a = 0; a <= 4; a++) {
+int main()
+{
+    for (int a = 0; a <= 4; a++)
+    {
         cout << "* ";
     }
     cout << endl;
 
-    for (int b = 0; b < 2; b++) {
-        for(int c = 0; c < 1; c++) {
+    for (int b = 0; b < 2; b++)
+    {
+        for (int c = 0; c < 1; c++)
+        {
             cout << "*";
         }
-        for (int d = 0; d <= 6; d++) {
+        for (int d = 0; d <= 6; d++)
+        {
             cout << " ";
         }
         cout << "*" << endl;
     }
 
-    for(int e = 0; e <= 4; e++) {
+    for (int e = 0; e <= 4; e++)
+    {
         cout << "* ";
     }
 
